d8aae8133e10a29372de2c0e89ed9cc6d807cdbb
rrossii <annros2003@gmail.com>
2023-10-18 19:38:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Run Program
+
+* create environment and install dependencies
+```
+    conda create --name <ENV_NAME> --file requirements.txt
+```
+
+* activate virtual environment
+```
+    conda activate <ENV_NAME>
+```
---
+++ military.py
@@ -0,0 +1,120 @@
+import cv2 as cv
+
+from keras.layers import Dense, Flatten, Dropout
+from keras.models import Model
+
+from glob import glob
+
+import numpy as np
+
+IMM_SIZE = 224
+
+
+def get_data(folder):
+    data = []
+    images = glob(folder + "/*")
+    class_name = folder.rsplit("/", 1)[-1]
+
+    for img in images:
+        image = cv.imread(img)
+        if image is not None:
+            image = cv.resize(image, (224, 224))
+        if image.shape[2] == 1:
+            image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+
+        data.append([image, class_name])
+
+    return data
+
+
+folder_military = "dataset/test/millitary"
+folder_other = "dataset/test/other"
+test_military = get_data(folder_military)
+test_other = get_data(folder_other)
+test = test_military + test_other
+
+folder_military = "dataset/train/millitary"
+folder_other = "dataset/train/other"
+train_millitary = get_data(folder_military)
+train_other = get_data(folder_other)
+train = train_millitary + train_other
+
+# folder_military = "dataset/validation/millitary"
+# folder_other = "dataset/validation/other"
+# val_millitary = get_data(folder_military)
+# val_other = get_data(folder_other)
+# val = val_millitary + val_other
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+x_val = []
+y_val = []
+
+for i in range(0, len(train)):
+    x_train.append(train[i][0]) #image
+    y_train.append(train[i][1]) #label
+for i in range(0, len(test)):
+    x_test.append(test[i][0])
+    y_test.append(test[i][1])
+# for i in range(0, len(val)):
+#     x_val.append(val[i][0])
+#     y_val.append(val[i][1])
+
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+# x_val = np.array(x_val)
+# y_val = np.array(y_val)
+
+from keras.applications import VGG16
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+x = base_model.output
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(128, activation='relu')(x)
+
+predictions = Dense(1, activation='sigmoid')(x)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+base_model.summary()
+
+model = Model(inputs=base_model.input, outputs=predictions)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3, validation_data=(x_train, y_train))
+for img in x_test:
+    prediction = model.predict(img)
+    print(prediction)
+
+
+def vehicleType(img_path):
+    img = cv.imread(img_path)
+
+    if img is not None:
+        img = cv.resize(img, (IMM_SIZE, IMM_SIZE))
+    if img.shape[2] == 1:
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+    pred = model.predict_classes(img)
+    return pred
+
+img = "dataset/test/other/other_0_1006.jpeg"
+classOfImage = vehicleType(img)
+
+print(f"{img} class is {classOfImage}")
+
+
+z = model.predict_classes(x_train) == y_train
+scores_train = sum(z+0) / len(z)
+z = model.predict_classes(x_test) == y_test
+scores_test = sum(z+0) / len(z)
+
+print('Train DataSet accuracy: {: .1%}'.format(scores_train), 'Test DataSet accuracy: {: .1%}'.format(scores_test))
